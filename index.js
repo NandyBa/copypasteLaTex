5fc092dfe7ac5abdbd3176c8abfabd7124ac13c9
Nandy Bâ <nandyba@yahoo.fr>
2021-01-29 00:18:46 +0100

    Supression des espaces inutiles

--- index.js
+++ index.js
@@ -2,4 +2,9 @@ code = document.getElementsByTagName('textarea')[0].value; // get code textarea 
 
 code = code.replace(/(\n[0-9]+)+\s*/g,'\n'); // remove numbers at begining of each row
 
+/* lines to remove unuseful spaces */
+code = code.replaceAll(/[ X]*\.[ X]*/g,'.');
+code = code.replaceAll(/[ X]*\([ X]*/g,'(');
+code = code.replaceAll(/[ X]*\)[ X]*/g,')');
+
 document.getElementsByTagName('div')[0].innerText = code; // show modified code
