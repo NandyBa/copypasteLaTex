f1fa4c6b39531df0b9fa96270f6e69e6a19e3895
Nandy Bâ <nandyba@yahoo.fr>
2021-01-29 21:17:53 +0100

    Ajout d'un boutton copier

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 $textarea = document.getElementsByTagName('textarea')[0];
 $div = document.getElementsByTagName('div')[0];
 $updateButton = document.getElementById('update');
+$copyButton = document.getElementById('copy');
+
+code = "";
 
 $updateButton.onclick = function() {
+	console.log("trigger");
 	code = $textarea.value; // get code textarea code
 
 	code = code.replace(/(\n[0-9]+)+\s*/g,'\n'); // remove numbers at begining of each row
@@ -16,4 +20,8 @@ $updateButton.onclick = function() {
 	code = code.replaceAll(/[ X]*\‘[ X]*/g,"'");
 
 	$div.innerText = code; // show modified code
+}
+
+$copyButton.onclick = function() {
+	navigator.clipboard.writeText(code);	// copy generated code in the clipboard
 }
