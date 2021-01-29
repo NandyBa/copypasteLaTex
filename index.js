2afc723fe9b3fd35c3c92498327e4c1145aeb87f
Nandy Bâ <nandyba@yahoo.fr>
2021-01-29 21:04:32 +0100

    AJout d'un boutton update

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
-code = document.getElementsByTagName('textarea')[0].value; // get code textarea code
+$textarea = document.getElementsByTagName('textarea')[0];
+$div = document.getElementsByTagName('div')[0];
+$updateButton = document.getElementById('update');
 
-code = code.replace(/(\n[0-9]+)+\s*/g,'\n'); // remove numbers at begining of each row
+$updateButton.onclick = function() {
+	code = $textarea.value; // get code textarea code
 
-/* lines to remove unuseful spaces */
-code = code.replaceAll(/[ X]*\.[ X]*/g,'.');
-code = code.replaceAll(/[ X]*\([ X]*/g,'(');
-code = code.replaceAll(/[ X]*\)[ X]*/g,')');
+	code = code.replace(/(\n[0-9]+)+\s*/g,'\n'); // remove numbers at begining of each row
 
+	/* lines to remove unuseful spaces */
+	code = code.replaceAll(/[ X]*\.[ X]*/g,'.');
+	code = code.replaceAll(/[ X]*\([ X]*/g,'(');
+	code = code.replaceAll(/[ X]*\)[ X]*/g,')');
 
-code = code.replaceAll(/[ X]*\’[ X]*/g,"'");
-code = code.replaceAll(/[ X]*\‘[ X]*/g,"'");
+	code = code.replaceAll(/[ X]*\’[ X]*/g,"'");
+	code = code.replaceAll(/[ X]*\‘[ X]*/g,"'");
 
-document.getElementsByTagName('div')[0].innerText = code; // show modified code
+	$div.innerText = code; // show modified code
+}
