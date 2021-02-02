2345bbca65aa9486df455a18bae8cc79e5cb967d
Nandy Bâ <nandyba@yahoo.fr>
2021-02-02 23:59:51 +0100

    Suppression des nombres en début de première ligne

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ code = "";
 $updateButton.onclick = function() {
 	code = $textarea.value; // get code textarea code
 
+	code = '\n'+code;
 	code = code.replace(/(\n[0-9]+)+\s*/g,'\n'); // remove numbers at begining of each row
+	code = code.substring(1) // to remove the first \n use to match number in the first row
 
 	/* lines to remove unuseful spaces */
 	code = code.replaceAll(/[ X]*\.[ X]*/g,'.');
